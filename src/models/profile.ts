97ae3bd9b96a7cccd59e1e5d5c66ea7edcfea239
fhalguni <ashpatil454@gmail.com>
2025-04-08 11:36:31 +0530

    git commit

--- src/models/profile.ts
+++ src/models/profile.ts
@@ -1,10 +1,4 @@
-import {
-  Column,
-  Entity,
-  ManyToOne,
-  OneToOne,
-  PrimaryGeneratedColumn,
-} from "typeorm";
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 import { User } from "./user";
 
 @Entity("profile_table11")
